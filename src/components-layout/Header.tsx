55b25ce49108c61de3087869f44bc904e976a172
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2024-03-22 15:11:21 +0100

    Update Header

--- src/components-layout/Header.tsx
+++ src/components-layout/Header.tsx
@@ -1,30 +1,3 @@
-import React, { useContext } from 'react';
-import { authContext } from '../contexts/AuthContext';
-
 export default function Header() {
-  const { isAuthenticated, login, logout } = useContext(authContext);
-
-  const handleLogin: React.MouseEventHandler<HTMLAnchorElement> = (e) => {
-    e.preventDefault();
-    login();
-  };
-
-  const handleLogout: React.MouseEventHandler<HTMLAnchorElement> = (e) => {
-    e.preventDefault();
-    logout();
-  };
-
-  return (
-    <div>
-      {isAuthenticated ? (
-        <a href='#' onClick={handleLogout}>
-          Logout
-        </a>
-      ) : (
-        <a href='#' onClick={handleLogin}>
-          Login
-        </a>
-      )}
-    </div>
-  );
+  return <div></div>;
 }
