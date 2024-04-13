536d28c5da926fb84ea0ec3c28aa61c86d263305
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2024-04-13 22:08:09 +0200

    Add tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'selector',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        textColor: {
+          darkMode: '#fff',
+          DEFAULT: '#333',
+        },
+        bgColor: {
+          darkMode: '#373737',
+          DEFAULT: '#fff',
+          appBg: '#fcfcfc',
+        },
+        gray: {
+          light: '#808080',
+          DEFAULT: '#373737',
+          dark: '#232323',
+        },
+        accent: 'var(--accent)',
+      },
+      height: {
+        lists: 'calc(100vh - 200px)',
+        // other properties...
+      },
+    },
   },
   plugins: [],
 };
