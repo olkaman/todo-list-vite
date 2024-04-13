b8e25bc87a6178c1a15a3cf086ae897bc31c2158
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2024-04-13 22:08:53 +0200

    Ad models

--- src/utils/models.ts
+++ src/utils/models.ts
@@ -1,3 +1,3 @@
-export type TodoList = { key: string; listId?: string; listName: string; todoList?: TodoItemType[] };
+export type TodoList = { key: number; listId?: string; listName: string; todoList?: TodoItemType[] };
 
 export type TodoItemType = { key: string; task: string; checked: boolean; date: number };
