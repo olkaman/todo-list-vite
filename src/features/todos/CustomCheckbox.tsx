5f617b0291494514f077ccb91dd60b9998697d97
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2024-05-07 22:27:12 +0200

    Fix checkbox styles

--- src/features/todos/CustomCheckbox.tsx
+++ src/features/todos/CustomCheckbox.tsx
@@ -16,12 +16,12 @@ function CustomCheckbox(props: Props) {
       onClick={handleOnCheck}
       disabled={disabled}
       className={clsx(
-        checked && 'bg-accent border-1 dark:border-2  dark:border-accent ',
+        checked && 'bg-accent border-1 dark:border-2 dark:border-accent',
         disabled && 'opacity-50 cursor-not-allowed',
-        'border border-gray-dark dark:border-gray-light rounded-full w-8 h-8 mr-3 globalTransition'
+        'border border-gray-dark dark:border-gray-light rounded-full p-3.5 mr-4 globalTransition relative'
       )}
     >
-      {checked ? <Check strokeWidth={strokeWidth} size={iconSize} className='text-gray-dark ml-1' /> : null}
+      {checked ? <Check strokeWidth={strokeWidth} size={iconSize} className='text-gray-dark ml-1 absolute top-1 left-0' /> : null}
     </button>
   )
 }
