4431ed1a5751e603a5233435cd6e1587aafe1c00
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2024-07-18 20:12:57 +0200

    Fix nesting problem

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -20,7 +20,7 @@ export const Modal = forwardRef<HTMLDialogElement, Props>((props, ref) => {
   return (
     <dialog ref={ref} className='bg-black-300 p-6 rounded-lg w-[450px] backdrop:bg-black/60'>
       <h3 className='mb-2 font-semibold'>{title}</h3>
-      <p className='mb-8'>{children}</p>
+      <span className='mb-8'>{children}</span>
       <div className='flex justify-end'>
         <Button styleType={ButtonStyleTypes.Secondary} handleOnClick={handleOnCancel} customStyles='mr-2'>
           {cancelButtonLabel}
